66967925e1e4e4a8c39f78a0a5ecd567e4bdf30f
dhruvnashdesai <dhruv@getonzuri.com>
2025-09-19 15:40:13 -0400

    updates to mobile landing

--- page-store-finder.php
+++ page-store-finder.php
@@ -15,7 +15,7 @@ if ($full_screen === 'yes') {
     <html <?php language_attributes(); ?>>
     <head>
         <meta charset="<?php bloginfo('charset'); ?>">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
         <title><?php wp_title('|', true, 'right'); ?></title>
         <?php wp_head(); ?>
     </head>
