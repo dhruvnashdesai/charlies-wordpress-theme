8f00f9f401abb435b0ca70f4f346d51102ee36b6
dhruvnashdesai <dhruv@getonzuri.com>
2025-09-30 13:37:43 -0400

    more updates to account positioning

--- charlie-stores/js/app.js
+++ charlie-stores/js/app.js
@@ -434,9 +434,9 @@ class CharlieStoreApp {
             accountX = cartX + 80; // 80px to the right
             accountY = cartY + 20; // Slightly below
         } else {
-            // Desktop: 200px directly below cart marker
+            // Desktop: 300px directly below cart marker
             accountX = cartX;
-            accountY = cartY + 200;
+            accountY = cartY + 300;
         }
 
         // Create account marker data
