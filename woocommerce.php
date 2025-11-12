b66902523cd23982bae29f75c082ba2e902d86bd
dhruvnashdesai <dhruv@getonzuri.com>
2025-11-12 14:02:18 -0500

    updates to product page styling

--- woocommerce.php
+++ woocommerce.php
@@ -9,13 +9,6 @@ get_header(); ?>
 <main class="charlies-main woocommerce-page">
     <div class="charlies-container">
 
-        <?php if (is_shop()) : ?>
-            <!-- Shop Page Header -->
-            <div class="shop-header">
-                <h1 class="page-title">Our Products</h1>
-                <p class="shop-description">Browse our complete selection of premium nicotine products.</p>
-            </div>
-        <?php endif; ?>
 
         <?php
         // WooCommerce content
