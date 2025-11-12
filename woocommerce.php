40229005f76017a8a45999ba9945d120324852c3
dhruvnashdesai <dhruv@getonzuri.com>
2025-11-12 14:05:09 -0500

    half change to debug

--- woocommerce.php
+++ woocommerce.php
@@ -4,7 +4,11 @@
  * This template is used for all WooCommerce pages (shop, product archives, etc.)
  */
 
-get_header(); ?>
+get_header();
+
+// DEBUG: Show which template is loading
+echo '<!-- DEBUG: woocommerce.php template is loading -->';
+?>
 
 <main class="charlies-main woocommerce-page">
     <div class="charlies-container">
